1763d20af3c8ad141e5becce60db53754d37b4fa
natepost <nathanielpost@gmail.com>
2018-05-31 19:44:47 -0400

    success at end of 2. cards persisting

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -7,13 +7,44 @@
     <div v-for="(card, index) in list.cards" class="card card-body">
       {{  card.name  }}
       </div>
-  </div>
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button v-on:click="submitMessages(list.id)" class="btn btn-secondary">Add</button>
+      </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
   props: ["original_lists"],
+  data: function() {
+    return {
+      messages: {},
+      lists: this.original_lists,
+    }
+  },
+
+  methods: {
+  submitMessages: function(list_id) {
+    var data = new FormData
+    data.append("card[list_id]", list_id)
+    data.append("card[name]", this.messages[list_id]),
+
+    Rails.ajax({
+      url: "/cards",
+      type: "POST",
+      data: data,
+      dataType: "json",
+      beforeSend: function() { return true },
+      success: (data) => {
+        const index = this.lists.findIndex(item => item.id == list_id);
+        this.lists[index].cards.push(data);
+        this.messages[list_id] = undefined;
+      }
+      });
+  }
+}
 }
 </script>
 
