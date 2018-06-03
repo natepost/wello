b4e645012bc05f9b338a9d236a3093633244034b
natepost <nathanielpost@gmail.com>
2018-06-03 13:01:40 -0400

    mini commit. open/close modal works... in progress... can't edit content in card yet...

--- app/javascript/components/card.vue
+++ app/javascript/components/card.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="card card-body mb-3">
+<div>
+  <div @click="editing=true" class="card card-body mb-3">
     {{  card.name  }}
   </div>
+
+  <div v-if='editing' class="modal-backdrop show"></div>
+
+  <div v-if='editing' @click="editing=false" class="modal show" style="display: block">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ card.name }}</h5>
+        </div>
+        <div class="modal-body">
+          <input v-model="name" class="form-control"></input>
+        </div>
+        <div class="modal-footer">
+          <button @click="save" type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -10,7 +31,13 @@ export default {
   data: function() {
     return {
       editing: false,
+      name: this.card.name,
     }
+  },
+
+  methods: {
+    closeModal: function() {},
+    save: function() {},
   }
 }
 </script>
